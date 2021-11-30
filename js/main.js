be53ae6bea059d7f1bd0f90584f0a263f6459fdf
Laise <laise18x@gmail.com>
2021-11-29 21:40:26 -0300

    Criação do js (função e classe)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log('Hello Jogo da Velha!')
+class TicTacToeGame {
+  __hasStarted = false
+
+  constructor() {
+    return {
+      start: this.start.bind(this)
+    }
+  }
+
+  start() {
+    if (!this.__hasStarted) {
+      console.log('Começando o jogo!')
+      this.__hasStarted = true
+    }
+  }
+}
+
+function createGame() {
+  const game = new TicTacToeGame()
+  game.start()
+}
+
+createGame()
