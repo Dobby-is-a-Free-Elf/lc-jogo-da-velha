7caa74f6d61664ed7201910f6f719577b6a74a82
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2021-12-03 22:38:40 -0300

    Estados iniciais do game (grid, jogadores e jogador atual); função de desenhar o jogo (__render()) e função do evento click do botão (__handleClickGrid(index))

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 class TicTacToeGame {
   __hasStarted = false
+  __grid = [
+    '', '', '',
+    '', '', '',
+    '', '', '',
+  ]
+  __players = ['X', 'O']
+  __currentPlayer = 0
 
   constructor() {
     return {
@@ -7,13 +14,39 @@ class TicTacToeGame {
     }
   }
 
+  __handleClickGrid(index) {
+    if (this.__grid[index] !== '') return
+
+    this.__grid[index] = this.__players[this.__currentPlayer]
+    this.__currentPlayer = (this.__currentPlayer === 0) ? 1 : 0
+    this.__render()
+  }
+
+  __render() {
+    const gridContainer = document.createElement('div')
+    const gameContainer = document.getElementById('game')
+
+    gameContainer.innerHTML = ''
+    gridContainer.classList.add('grid')
+
+    this.__grid.forEach((item, index) => {
+      const button = document.createElement('button')
+      button.innerText = item
+      button.addEventListener('click', () => this.__handleClickGrid(index))
+      gridContainer.appendChild(button)
+    })
+
+    gameContainer.appendChild(gridContainer)
+  }
+
   start() {
-    if (!this.__hasStarted) {
-      console.log('Começando o jogo!')
-      this.__hasStarted = true
-    }
+    if (this.__hasStarted) return
+    
+    this.__render()
+    this.__hasStarted = true
   }
 }
+
 
 function createGame() {
   const game = new TicTacToeGame()
