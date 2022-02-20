684bd85b412fb566e7c6653790766b1b4a41c64e
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2022-02-20 20:09:10 -0300

    Exibição de jogador vitorioso, bloqueio do jogo caso ocorra vitória

--- js/classes/TicTacToeGame.js
+++ js/classes/TicTacToeGame.js
@@ -1,23 +1,23 @@
 export default class TicTacToeGame {
-  __hasStarted = false;
-  __grid = ["", "", "", "", "", "", "", "", ""];
-  __players = ["X", "O"];
-  __currentPlayer = 0;
-  __victory = false;
+  __hasStarted = false
+  __grid = ["", "", "", "", "", "", "", "", ""]
+  __players = ["X", "O"]
+  __currentPlayer = 0
+  __victory = false
 
   constructor(container) {
-    this.__gameContainer = container;
-    this.__gameContainer.classList.add("game-container");
+    this.__gameContainer = container
+    this.__gameContainer.classList.add("game-container")
 
     return {
       start: this.start.bind(this),
-    };
+    }
   }
 
   __verifyVictory() {
     return (
       this.__verifyRows() || this.__verifyColumns() || this.__verifyDiagonals()
-    );
+    )
   }
 
   __verifyRows() {
@@ -26,7 +26,7 @@ export default class TicTacToeGame {
         this.__grid[index] !== "" &&
         this.__grid[index] === this.__grid[index + 1] &&
         this.__grid[index] === this.__grid[index + 2]
-    );
+    )
   }
 
   __verifyColumns() {
@@ -35,7 +35,7 @@ export default class TicTacToeGame {
         this.__grid[index] !== "" &&
         this.__grid[index] === this.__grid[index + 3] &&
         this.__grid[index] === this.__grid[index + 6]
-    );
+    )
   }
 
   __verifyDiagonals() {
@@ -46,58 +46,62 @@ export default class TicTacToeGame {
       (this.__grid[2] !== "" &&
         this.__grid[2] === this.__grid[4] &&
         this.__grid[2] === this.__grid[6])
-    );
+    )
   }
 
   __handleClickGrid(index) {
-    if (this.__grid[index] !== "") return;
+    if (this.__grid[index] !== "") return
+    if (this.__victory) return
 
-    this.__grid[index] = this.__players[this.__currentPlayer];
-    this.__currentPlayer = this.__currentPlayer === 0 ? 1 : 0;
-    this.__victory = this.__verifyVictory();
-    console.log(this.__victory);
-    this.__render();
+    this.__grid[index] = this.__players[this.__currentPlayer]
+    
+    this.__victory = this.__verifyVictory()
+    console.log(this.__victory)
+    
+    this.__render()
+
+    this.__currentPlayer = this.__currentPlayer === 0 ? 1 : 0
   }
 
   __render() {
-    this.__gameContainer.innerHTML = "";
+    this.__gameContainer.innerHTML = ""
 
-    const gridContainer = this.__renderGrid();
-    const playerDisplay = this.__renderPlayerDisplay();
+    const gridContainer = this.__renderGrid()
+    const playerDisplay = this.__renderPlayerDisplay()
 
-    this.__gameContainer.appendChild(playerDisplay);
-    this.__gameContainer.appendChild(gridContainer);
+    this.__gameContainer.appendChild(playerDisplay)
+    this.__gameContainer.appendChild(gridContainer)
   }
 
   __renderGrid() {
-    const gridContainer = document.createElement("div");
-    gridContainer.classList.add("grid");
+    const gridContainer = document.createElement("div")
+    gridContainer.classList.add("grid")
 
     this.__grid.forEach((item, index) => {
-      const button = document.createElement("button");
-      button.innerText = item;
-      button.addEventListener("click", () => this.__handleClickGrid(index));
-      gridContainer.appendChild(button);
-    });
+      const button = document.createElement("button")
+      button.innerText = item
+      button.addEventListener("click", () => this.__handleClickGrid(index))
+      gridContainer.appendChild(button)
+    })
 
-    return gridContainer;
+    return gridContainer
   }
 
   __renderPlayerDisplay() {
-    const displayPlayerContainer = document.createElement("div");
-    const span = document.createElement("span");
-    span.classList.add("player-display");
-    span.innerText = `Player: ${this.__players[this.__currentPlayer]}`;
+    const displayPlayerContainer = document.createElement("div")
+    const span = document.createElement("span")
+    span.classList.add("player-display")
+    span.innerText = `${this.__victory ? 'Vencedor' : ' Player'}: ${this.__players[this.__currentPlayer]}`
 
-    displayPlayerContainer.appendChild(span);
+    displayPlayerContainer.appendChild(span)
 
-    return displayPlayerContainer;
+    return displayPlayerContainer
   }
 
   start() {
-    if (this.__hasStarted) return;
+    if (this.__hasStarted) return
 
-    this.__render();
-    this.__hasStarted = true;
+    this.__render()
+    this.__hasStarted = true
   }
 }
